0e6eb472235b7b7fb5979374e1da55ccd609faa2
PetkoPopov <poetpoet2014@mail.ru>
2021-02-17 16:33:17 +0200

    add pad and other staff

--- public_html/newjavascript1.js
+++ public_html/newjavascript1.js
@@ -3,13 +3,15 @@ var a = document.getElementsByTagName('div')[0];
 var txt = '';
 var speed = 100;
 var level = 0;
-
+var showYourScore = document.getElementById("score");
 var canv = document.getElementById("workField");
 canv.style.border = "solid black 3px";
 var workField = canv.getContext("2d");
 var nickName = '';
 var game = {};
 var isPassed = false;
+var allBullets = [];
+var count = 0;
 var inputLevel = document.getElementsByTagName('input')[0];
 inputLevel.style.backgroundColor = "lightgreen";
 inputLevel.addEventListener('keypress', function (e) {
@@ -20,23 +22,38 @@ inputLevel.addEventListener('keypress', function (e) {
 //////////////////////////////////
 window.addEventListener('mousemove', function (mouse) {
     if (allBullets.length > 0) {
-       
+
         for (let y = 0; y < allBullets.length; y++) {
             if (allBullets[y].coorX <= mouse.clientX - 8 && allBullets[y].coorX + 5 >= mouse.clientX - 8) {
                 if (allBullets[y].coorY <= mouse.clientY - 50 && allBullets[y].coorY + 5 >= mouse.clientY - 50) {
-                    console.log("SUCCESS", mouse.clientX, mouse.clientY, allBullets[y]);
+//                    console.log("SUCCESS", mouse.clientX, mouse.clientY, allBullets[y]);
                     workField.clearRect(0, 0, 500, 200);
                     score++;
 
-                    a.style.backgroundColor = "# 447766";
-                    a.innerHTML = "Your score" + score;
+
+                    //                    a.style.backgroundColor = "# 447766";
+//                    console.log(showYourScore);
+                    showYourScore.textContent = "Your score " + score;
+
                     allBullets.splice(y, 1);
                 }
             }
         }
     }
-    
+
 });
+window.addEventListener('keydown', function (button) {
+
+    if (button.key == "ArrowDown") {
+        movePadDown();
+
+    } else if (button.key == "ArrowUp") {
+        movePadUp();
+
+    }
+
+})
+
 
 var coor = 16;
 var score = 0;
@@ -46,28 +63,30 @@ function moveAllBullets() {
             workField.clearRect(allBullets[int].coorX, allBullets[int].coorY, 10, 10);    // изчиства полето
             allBullets[int].coorX++;
             workField.fillRect(allBullets[int].coorX, allBullets[int].coorY, 10, 10);
-            if (allBullets[int].coorX >= 500) {
+            let x = allBullets[int].coorX;
+            let y = allBullets[int].coorY;
+            if (match()) {
                 allBullets.splice(int, 1);
-                score--;
-                console.log(allBullets);
+                workField.clearRect(x, y, 10, 10);
             }
+            if (allBullets.length > 0) {
+                if (allBullets[int].coorX >= 500) {
+                    allBullets.splice(int, 1);
+                    score--;
+                }
 
-
-
+            }
+            moveAllBullets();
         }
-
-        moveAllBullets();
-
     }, speed);
 }
 
 
 ///////////////////////
-var allBullets = [];
-var count = 0;
+
 function createBullet() {
 
-    let num = Math.ceil(Math.random() * 200);
+    let num = Math.ceil(Math.random() * 100);
     let bullet = {
         "start": 0,
         "count": 1
@@ -86,25 +105,88 @@ function startLevel() {
         createBullet();
 
     }
-    speed--;
+//    speed--;
     moveAllBullets();
+    pad();
+
 }
 
 ///////////////////////////////////////////////////
 function main() {
     a.style.border = "3px solid blue "
-    a.style.backgroundColor ="blue";
+    a.style.backgroundColor = "blue";
     a.innerHTML = "You pass the level " + level;
-    if(allBullets.length == 0)
-    setTimeout(function () {
-        level++;
-        startLevel();
-    }, 3000)
+    if (allBullets.length == 0)
+        setTimeout(function () {
+            level++;
+            startLevel();
+        }, 3000)
 }
+////////////
+var padRect = {};
+padRect.allCoor = [];
 
+function pad() {
+    for (let r = 0; r < 5; r++) {
 
+        let aid = {
+            coorY: r * 5,
+            coorX: 493
+        };
+        padRect.allCoor.push(aid);
+        showPad();
 
+    }
 
+}
+function showPad() {
+    for (let r = 0; r < padRect.allCoor.length; r++) {
+        let x = padRect.allCoor[r].coorX;
+        let y = padRect.allCoor[r].coorY;
+        workField.fillRect(x, y, 5, 5);
+    }
+}
+/////////////////////////////////
+function movePadDown() {
+    let hlp = {
+        coorX: padRect.allCoor[0].coorX,
+        coorY: padRect.allCoor[0].coorY + 25
+    }
+
+    workField.clearRect(hlp.coorX, hlp.coorY - 25, 5, 5);
+    padRect.allCoor.splice(0, 1)
+    padRect.allCoor.push(hlp);
+
+    showPad();
+}
+function movePadUp() {
+    let hlp = {
+        coorX: padRect.allCoor[padRect.allCoor.length - 1].coorX,
+        coorY: padRect.allCoor[padRect.allCoor.length - 1].coorY - 25
+    }
+
+    workField.clearRect(padRect.allCoor[padRect.allCoor.length - 1].coorX, padRect.allCoor[padRect.allCoor.length - 1].coorY, 5, 5);
+    padRect.allCoor.pop();
+    padRect.allCoor.splice(0, 0, hlp);
+
+    showPad();
+}
+function movePadLeft() {
+}
+function movePadRight() {
+}
+function match() {
+    for (let r = 0; r < allBullets.length; r++) {
+
+        if (allBullets[r].coorX == 483) {
+            if (allBullets[r].coorY >= padRect.allCoor[0].coorY && allBullets[r].coorY <= padRect.allCoor[0].coorY + 25) {
+                console.log("you catched :) ")
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 
 
